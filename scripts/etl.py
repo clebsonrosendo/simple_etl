e773371c93b5166c109bef13d3a1687b71884ac4
Clebson Rosendo <clebsondev@gmail.com>
2025-01-09 16:31:14 -0300

    refactor(all): finally

--- scripts/etl.py
+++ scripts/etl.py
@@ -1,66 +1,40 @@
 import pandas as pd
+import json
 
 
-# Recebe os dados e transforma em um Pandas DataFrame
-class ExtractData():
+data = './files/data.xlsx'
 
-    def __init__(self) -> None:
-        pass
+df = pd.read_excel(data, dtype=str, na_values='')
+df = df.where(pd.notnull(df), '')
+df['Nome'] = df['Nome'].str.upper()
+df['CPF'] = df['CPF'].replace(r'[^0-9\s]', '', regex=True)
+df['Período'] = df['Período'].replace(r'[^0-9\s]', '', regex=True)
+df['Matrícula'] = df['Matrícula'].replace(r'[^0-9\s]', '', regex=True)
+df['Título'] = df['Título'].replace(r'[^0-9\s]', '', regex=True)
+df['Endereco_CEP'] = df['Endereco_CEP'].astype(str)
 
-    def Start(self, df) -> pd.DataFrame:
-        data = pd.DataFrame(df)
-        return data
+json_data = df.apply(
+    lambda row: {
+    'nome': row['Nome'],
+    'cpf': row['CPF'],
+    'endereco': {
+        'logradouro': row['Endereco_logradouro'],
+        'numero': row['Endereco_num'],
+        'bairro': row['Endereco_bairro'],
+        'complemento': row['Endereco_complemento'],
+        'cidade': row['Endereco_cidade'],
+        'uf': row['Endereco_UF'],
+        'cep': row['Endereco_CEP']
+    },
+    'email': row['Email'],
+    'celular': row['Celular'],
+    'instituicao': row['Instituição de Ensino'],
+    'periodo': row['Período'],
+    'matricula': row['Matrícula'],
+    'título': row['Título']
+    },
+    axis=1
+).tolist()
 
-
-# Responsável por realizar as transformações 
-class TransformData():
-
-    def __init__(self):
-        pass
-
-    def ToString(self, df):
-        dfToString = pd.DataFrame(df)
-        dfToString.astype(str)
-        return dfToString
-    
-    def RemovePointsCPF():
-        return
-
-    def JustCellNumber():
-        return
-
-    def JustNumberCoursePeriod():
-        return
-
-    def SplitAddress():
-        return
-
-
-# Carrega os dados
-class LoadData():
-
-    def __init__(self):
-        pass
-
-# Suporte a conversão para diferenter formatos de arquivos
-class ConvertFile():
-
-    def __init__(self):
-        pass
-
-    def ToJson(self, df):
-        df = pd.DataFrame(df)
-        df.to_json()
-        return df
-
-    def ToSql():
-        return
-    
-    def ToXlsx():
-        return
-
-    def ToTxt():
-        return
-    
-    def ToCsv():
-        return
+with open('./files/data.json', 'w', encoding='utf-8') as f:
+    json.dump(json_data, f, ensure_ascii=False, indent=4)
